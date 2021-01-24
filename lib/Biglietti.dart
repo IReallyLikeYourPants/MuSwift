3e6edd339d7718ca2349db3f84e747f22707c462
Quelehir <mkryston2@gmail.com>
2021-01-24 19:05:37 +0100

    Update Biglietti.dart
    biglietti ho messo dio al posto di biglietti ho messo dio

--- lib/Biglietti.dart
+++ lib/Biglietti.dart
@@ -15,7 +15,7 @@ class _HomeState extends State<Biglietti> {
           title: new Text("Biglietti"),
         ),
         body: new Center(
-            child: new Text("this is biglietti")
+            child: new Text("this is dio")
         )
     );
   }
