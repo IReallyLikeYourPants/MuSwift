75d31d5759dfaa6d9e38b06e2c0368aef367c958
r4lpf <a.r.almoneda@gmail.com>
2021-01-30 12:00:25 +0100

    adesso la ricerca funziona
    quello sopra
    
    Co-Authored-By: PercivallAkihiko <37913210+PercivallAkihiko@users.noreply.github.com>

--- lib/Ricerca.dart
+++ lib/Ricerca.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
 import 'package:flappy_search_bar/flappy_search_bar.dart';
 import 'package:flappy_search_bar/scaled_tile.dart';
 import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:sqflite/sqflite.dart';
+
+import 'museumPage.dart';
+
+
+
 
 class Post {
   final String title;
@@ -18,12 +25,8 @@ class Ricerca extends StatefulWidget {
 
 }
 
-
-
-
 class StorageUploadState extends State<Ricerca> {
   List results = [];
-
   var rows = [];
   String query = '';
   TextEditingController tc;
@@ -32,132 +35,157 @@ class StorageUploadState extends State<Ricerca> {
   void initState() {
     super.initState();
     tc = TextEditingController();
-    rows = [
-      {
-        'img': 'assets/images/deposizione.jpg',
-        'contact_name': 'Test User 1',
-        'contact_phone': '066 560 4900',
-      },
-      {
-        'img': 'assets/images/deposizione.jpg',
-        'contact_name': 'Test User 2',
-        'contact_phone': '066 560 7865',
-      },
-      {
-        'img': 'assets/images/deposizione.jpg',
-        'contact_name': 'Test User 3',
-        'contact_phone': '906 500 4334',
-      }
-    ];
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.black87,
-      appBar: new AppBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.search),color: Colors.white,),
-        title: new Text(
-          "Search",
-          style: new TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Stack(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Theme(
-                      child: TextField(
-                          //style: TextStyle(color: Colors.grey),
-                          controller: tc,
-                          decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search),
-                            hintText: 'Search...',
-                            hintStyle: TextStyle(color: Colors.grey)
-                          ),
-                          onChanged: (v) {
-                          setState(() {
-                            query = v;
-                            setResults(query);
-                          });
-                    },
-                  ),
-                    data: new ThemeData( //colore della search bar
-                        primaryColor: Colors.white,
-                        accentColor: Colors.orange,
-                        hintColor: Colors.white
-                    ),
-                  ),
-                ),
-                Container(
-                  color: Colors.black12,
-                  child: query.isEmpty
-                      ? ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: rows.length,
-                    itemBuilder: (con, ind) {
-                      return Card (color: Colors.black12,child: ListTile(
-                        title: Text(rows[ind]['contact_name'],style: TextStyle(color: Colors.white)),
-                        subtitle: Text(rows[ind]['contact_phone'],style: TextStyle(color: Colors.white54)),
+    return  FutureBuilder(
+        future: DefaultAssetBundle.of(context).loadString('assets/loadjson/musei.json'),
+        builder: (context, snapshot){
 
-                        onTap: () {
-                          setState(() {
-                            tc.text = rows[ind]['contact_name'];
-                            query = rows[ind]['contact_name'];
-                            setResults(query);
-                          });
-                        },
-                      ));
-                    },
-                  )
-                      : ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: results.length,
-                    itemBuilder: (con, ind) {
-                      return Card(color: Colors.black12,child: ListTile(
-                        title: Text(results[ind]['contact_name'],style: TextStyle(color: Colors.white)),
-                        subtitle: Text(results[ind]['contact_phone'],style: TextStyle(color: Colors.white54)),
+          var rows = json.decode(snapshot.data.toString());
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: new AppBar(
+              backgroundColor: Colors.black54,
+              //leading: IconButton(
+              //icon: Icon(Icons.search),color: Colors.white,),
+              title: new Text(
+                "MuSwift",
+                style: new TextStyle(
+                  color: Colors.white.withOpacity(0.4),
 
-                        onTap: () {
-                          setState(() {
-                            tc.text = results[ind]['contact_name'];
-                            query = results[ind]['contact_name'];
-                            setResults(query);
-                          });
-                        },
-                      ));
-                    },
-                  ),
                 ),
-              ],
+              ),
             ),
-          ],
-        ),
-      ),
+            body: Container(
+              padding: EdgeInsets.all(10),
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Theme(
+                          child: TextField(
+                            //style: TextStyle(color: Colors.grey),
+                            controller: tc,
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.search),
+                                hintText: 'Search...',
+                                hintStyle: TextStyle(color: Colors.white)
+                            ),
+                            onChanged: (v) {
+                              setState(() {
+                                query = v;
+                                setResults(query,rows);
+                              });
+                            },
+                          ),
+                          data: new ThemeData( //colore della search bar
+                              primaryColor: Colors.black54,
+                              accentColor: Colors.orange,
+                              hintColor: Colors.black54
+                          ),
+                        ),
+                      ),
+
+
+                      query.isEmpty ?
+                      Expanded(
+                          child: Column(children : [
+                            SizedBox(width: double.infinity,child: Text("  Recenti",style: TextStyle(color: Colors.black54,height: 2,fontSize: 23),textAlign: TextAlign.left)),
+                            Divider(color: Colors.black54,),
+                            Expanded (child :
+                            ListView.builder(
+                              shrinkWrap: true,
+                              itemCount: rows == null ? 0 : rows.length,
+                              itemBuilder: (con, ind) {
+                                return Card (color: Colors.black12,
+                                    child: ListTile(
+                                      leading: ConstrainedBox(
+                                        constraints: BoxConstraints(
+                                          minWidth: 60,
+                                          minHeight: 60,
+                                          maxWidth: 60,
+                                          maxHeight: 60,
+                                        ),
+                                        child: Image.asset(rows[ind]['img'].toString(), fit: BoxFit.cover),
+                                      ),
+                                      title: Text(rows[ind]['title'],style: TextStyle(color: Colors.white)),
+                                      subtitle: Text(rows[ind]['rate'].toString(),style: TextStyle(color: Colors.white54)),
+
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) => museumPage(
+                                                  rows[ind]['title'],
+                                                  rows[ind]['img'],
+                                                  rows[ind]['rate'],
+                                                )
+                                            )
+                                        );
+                                      },
+                                    ));
+                              },
+                            ))]))
+                          : Expanded(
+                          child:ListView.builder(
+                            shrinkWrap: true,
+                            itemCount: results.length,
+                            itemBuilder: (con, ind) {
+                              return Card(color: Colors.black12,child: ListTile(
+                                leading: ConstrainedBox(
+                                  constraints: BoxConstraints(
+                                    minWidth: 60,
+                                    minHeight: 60,
+                                    maxWidth: 60,
+                                    maxHeight: 60,
+                                  ),
+                                  child: Image.asset(rows[ind]['img'].toString(), fit: BoxFit.cover),
+                                ),
+                                title: Text(results[ind]['title'],style: TextStyle(color: Colors.white)),
+                                subtitle: Text(results[ind]['rate'].toString(),style: TextStyle(color: Colors.white54)),
+
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => museumPage(
+                                            rows[ind]['title'],
+                                            rows[ind]['img'],
+                                            rows[ind]['rate'],
+                                          )
+                                      )
+                                  );
+                                },
+                              ));
+                            },
+                          )
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
     );
   }
 
-  void setResults(String query) {
-    results = rows
+  void setResults(String query,var rowss) {
+    results = rowss
         .where((elem) =>
-    elem['contact_name']
+    elem['title']
         .toString()
         .toLowerCase()
         .contains(query.toLowerCase()) ||
-        elem['contact_phone']
+        elem['rate'].toString()
             .toString()
             .toLowerCase()
             .contains(query.toLowerCase()))
         .toList();
+
   }
 }
