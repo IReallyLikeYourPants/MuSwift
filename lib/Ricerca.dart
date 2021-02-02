5690235daf09f7b75c14c8a2cfe0501f92a94802
r4lpf <a.r.almoneda@gmail.com>
2021-02-02 10:37:30 +0100

    yo messo la tab bar sulla ricerca funzionante
    quello sul titolo

--- lib/Ricerca.dart
+++ lib/Ricerca.dart
@@ -9,6 +9,20 @@ import 'package:sqflite/sqflite.dart';
 
 import 'museoPage.dart';
 
+const appbarColor = Colors.black54;
+const backroundColor = Colors.white;
+
+const searchbarPrimaryColor = Colors.black;
+const searchbarHintColor = Colors.black;
+const searchbarTextColor = Colors.black;
+const searchbarText = "Search...";
+
+const listviewCardColor = Colors.grey;
+const listviewTitleColor = Colors.white;
+const listviewSubtitleColor = Colors.white;
+const double listviewImgSize = 60;
+
+
 class Post {
   final String title;
   final String description;
@@ -22,7 +36,12 @@ class Ricerca extends StatefulWidget {
 
 }
 
-class StorageUploadState extends State<Ricerca> {
+
+
+class StorageUploadState extends State<Ricerca> with TickerProviderStateMixin{
+
+  TabController _tabController;
+
   List results = [];
   var rows = [];
   String query = '';
@@ -32,6 +51,13 @@ class StorageUploadState extends State<Ricerca> {
   void initState() {
     super.initState();
     tc = TextEditingController();
+    _tabController = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
   }
 
   @override
@@ -42,9 +68,9 @@ class StorageUploadState extends State<Ricerca> {
 
           var rows = json.decode(snapshot.data.toString());
           return Scaffold(
-            backgroundColor: Colors.white,
+            backgroundColor: backroundColor,
             appBar: new AppBar(
-              backgroundColor: Colors.black54,
+              backgroundColor: appbarColor,
               //leading: IconButton(
               //icon: Icon(Icons.search),color: Colors.white,),
               title: new Text(
@@ -69,8 +95,8 @@ class StorageUploadState extends State<Ricerca> {
                             controller: tc,
                             decoration: InputDecoration(
                                 prefixIcon: Icon(Icons.search),
-                                hintText: 'Search...',
-                                hintStyle: TextStyle(color: Colors.white)
+                                hintText: searchbarText,
+                                hintStyle: TextStyle(color: searchbarTextColor)
                             ),
                             onChanged: (v) {
                               setState(() {
@@ -80,19 +106,17 @@ class StorageUploadState extends State<Ricerca> {
                             },
                           ),
                           data: new ThemeData( //colore della search bar
-                              primaryColor: Colors.black54,
-                              accentColor: Colors.orange,
-                              hintColor: Colors.black54
+                              primaryColor: searchbarPrimaryColor,
+                              //accentColor: Colors.orange,
+                              hintColor: searchbarHintColor
                           ),
                         ),
-                      ),
-
-
+                      ),  //SearchBar
                       query.isEmpty ?
                       Expanded(
                           child: Column(children : [
-                            SizedBox(width: double.infinity,child: Text("  Recenti",style: TextStyle(color: Colors.black54,height: 2,fontSize: 23),textAlign: TextAlign.left)),
-                            Divider(color: Colors.black54,),
+                            //SizedBox(width: double.infinity,child: Text("  Recenti",style: TextStyle(color: Colors.black54,height: 2,fontSize: 23),textAlign: TextAlign.left)),
+                            //Divider(color: Colors.black54,),
                             Expanded (child :
                             ListView.builder(
                               shrinkWrap: true,
@@ -102,15 +126,15 @@ class StorageUploadState extends State<Ricerca> {
                                     child: ListTile(
                                       leading: ConstrainedBox(
                                         constraints: BoxConstraints(
-                                          minWidth: 60,
-                                          minHeight: 60,
-                                          maxWidth: 60,
-                                          maxHeight: 60,
+                                          minWidth: listviewImgSize,
+                                          minHeight: listviewImgSize,
+                                          maxWidth: listviewImgSize,
+                                          maxHeight: listviewImgSize,
                                         ),
                                         child: Image.asset(rows[ind]['img'].toString(), fit: BoxFit.cover),
                                       ),
-                                      title: Text(rows[ind]['title'],style: TextStyle(color: Colors.white)),
-                                      subtitle: Text(rows[ind]['rate'].toString(),style: TextStyle(color: Colors.white54)),
+                                      title: Text(rows[ind]['title'],style: TextStyle(color: listviewTitleColor)),
+                                      subtitle: Text(rows[ind]['rate'].toString(),style: TextStyle(color: listviewSubtitleColor)),
 
                                       onTap: () {
                                         Navigator.push(
@@ -128,39 +152,152 @@ class StorageUploadState extends State<Ricerca> {
                               },
                             ))]))
                           : Expanded(
-                          child:ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: results.length,
-                            itemBuilder: (con, ind) {
-                              return Card(color: Colors.black12,child: ListTile(
-                                leading: ConstrainedBox(
-                                  constraints: BoxConstraints(
-                                    minWidth: 60,
-                                    minHeight: 60,
-                                    maxWidth: 60,
-                                    maxHeight: 60,
+                          child: Column(children : [
+                            //SizedBox(width: double.infinity,child: Text("  Recenti",style: TextStyle(color: Colors.black54,height: 2,fontSize: 23),textAlign: TextAlign.left)),
+                            //Divider(color: Colors.black54,),
+                            TabBar(
+                              controller: _tabController,
+                              indicatorColor: Colors.blue,
+                              labelColor: Colors.blue,
+                              unselectedLabelColor: Colors.black,
+                              tabs: <Widget>[
+                                Tab(
+                                  child: Text(
+                                    "TUTTO",
+                                    style: TextStyle(
+                                        fontSize: 17
+                                    ),
                                   ),
-                                  child: Image.asset(rows[ind]['img'].toString(), fit: BoxFit.cover),
-                                ),
-                                title: Text(results[ind]['title'],style: TextStyle(color: Colors.white)),
-                                subtitle: Text(results[ind]['rate'].toString(),style: TextStyle(color: Colors.white54)),
+                                ), // TUTTO TAB
+                                Tab(
+                                  child: Text(
+                                    "MUSEI",
+                                    style: TextStyle(
+                                        fontSize: 17
+                                    ),
+                                  ),
+                                ), // MUSEI TAB
+                                Tab(
+                                  child: Text(
+                                    "OPERE",
+                                    style: TextStyle(
+                                        fontSize: 17
+                                    ),
+                                  ),
+                                ), // OPERE TAB
+                              ],
+                            ),
+                            Expanded(
+                                child: TabBarView(
+                                    controller : _tabController,
+                                    children: [
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount: results.length,
+                                        itemBuilder: (con, ind) {
+                                          return Card(color: listviewCardColor,child: ListTile(
+                                            leading: ConstrainedBox(
+                                              constraints: BoxConstraints(
+                                                minWidth: listviewImgSize,
+                                                minHeight: listviewImgSize,
+                                                maxWidth: listviewImgSize,
+                                                maxHeight: listviewImgSize,
+                                              ),
+                                              child: Image.asset(results[ind]['img'].toString(), fit: BoxFit.cover),
+                                            ),
+                                            title: Text(results[ind]['title'],style: TextStyle(color: listviewTitleColor)),
+                                            subtitle: Text(results[ind]['rate'].toString(),style: TextStyle(color: listviewSubtitleColor)),
 
-                                onTap: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => museoPage(
-                                            rows[ind]['title'],
-                                            rows[ind]['img'],
-                                            rows[ind]['rate'],
-                                          )
-                                      )
-                                  );
-                                },
-                              ));
-                            },
-                          )
-                      ),
+                                            onTap: () {
+                                              Navigator.push(
+                                                  context,
+                                                  MaterialPageRoute(
+                                                      builder: (context) => museoPage(
+                                                        rows[ind]['title'],
+                                                        rows[ind]['img'],
+                                                        rows[ind]['rate'],
+                                                      )
+                                                  )
+                                              );
+                                            },
+                                          ));
+                                        },
+                                      ), // TUTTO TAB
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount: results.length,
+                                        itemBuilder: (con, ind) {
+                                          if (results[ind]["tipo"] == "museo" ){
+                                            return Card(color: listviewCardColor,child: ListTile(
+                                              leading: ConstrainedBox(
+                                                constraints: BoxConstraints(
+                                                  minWidth: listviewImgSize,
+                                                  minHeight: listviewImgSize,
+                                                  maxWidth: listviewImgSize,
+                                                  maxHeight: listviewImgSize,
+                                                ),
+                                                child: Image.asset(results[ind]['img'].toString(), fit: BoxFit.cover),
+                                              ),
+                                              title: Text(results[ind]['title'],style: TextStyle(color: listviewTitleColor)),
+                                              subtitle: Text(results[ind]['rate'].toString(),style: TextStyle(color: listviewSubtitleColor)),
+
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) => museoPage(
+                                                          rows[ind]['title'],
+                                                          rows[ind]['img'],
+                                                          rows[ind]['rate'],
+                                                        )
+                                                    )
+                                                );
+                                              },
+                                            ));
+                                          }
+                                          return Container();
+                                        },
+                                      ), // MUSEI TAB
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        itemCount: results.length,
+                                        itemBuilder: (con, ind) {
+                                          if (results[ind]["tipo"] == "opera" ){
+                                            return Card(color: listviewCardColor,child: ListTile(
+                                              leading: ConstrainedBox(
+                                                constraints: BoxConstraints(
+                                                  minWidth: listviewImgSize,
+                                                  minHeight: listviewImgSize,
+                                                  maxWidth: listviewImgSize,
+                                                  maxHeight: listviewImgSize,
+                                                ),
+                                                child: Image.asset(results[ind]['img'].toString(), fit: BoxFit.cover),
+                                              ),
+                                              title: Text(results[ind]['title'],style: TextStyle(color: listviewTitleColor)),
+                                              subtitle: Text(results[ind]['rate'].toString(),style: TextStyle(color: listviewSubtitleColor)),
+
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) => museoPage(
+                                                          rows[ind]['title'],
+                                                          rows[ind]['img'],
+                                                          rows[ind]['rate'],
+                                                        )
+                                                    )
+                                                );
+                                              },
+                                            ));
+                                          }
+                                          return Container();
+                                        },
+                                      ), // OPERE TAB
+                                    ]
+                                )
+                            )
+
+                          ])),
                     ],
                   ),
                 ],
@@ -186,3 +323,4 @@ class StorageUploadState extends State<Ricerca> {
 
   }
 }
+
